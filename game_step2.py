93d2aeb69de07fa418170ec9d35bb20ca03eda9b
gatupovmikhail <47658580+gatupovmikhail@users.noreply.github.com>
2019-04-17 12:24:08 +0300

    Окно для определения аминокислот
    Удалось решить проблему со вставкой изображений. Сделан начальный  интерфейс игры для изучения состава аминокислот.

--- game_step2.py
+++ game_step2.py
@@ -9,36 +9,54 @@ aminok= Tk()
 
 aminok.geometry('1650x900')
 aminok.title("Запоминание аминокислот")
-canvas = Canvas(aminok, width=1650, height=900, bg='green') # Холст. 0,0 - верхний левый угол.
+canvas = Canvas(aminok, width=1650, height=900, bg='green')
+# Холст. 0,0 - верхний левый угол.
 #####################################
 # ##################################################
 title=Label(aminok,text='Выберите \n аланин')
 title.config(bg='green',fg='white')
 title.config(font=('helvetica', 40, 'bold italic'))
 canvas.create_window(850,100, window=title)
-#photo=PhotoImage(file='lena.gif')      #/home/gatupov/PycharmProjects/venv
-#canvas.create_image(10, 10, image=photo, anchor=NW) # встроить изображение
+photo1=PhotoImage(file='tyr.gif')
+photo2=PhotoImage(file='gly.gif')
+photo3=PhotoImage(file='ile.gif')
+photo4=PhotoImage(file='pro.gif')
+photo5=PhotoImage(file='met.gif')
+photo6=PhotoImage(file='thr.gif')
+#/home/gatupov/PycharmProjects/venv
+#canvas.create_image(0, 0, image=photo, anchor=NW) # встроить изображение
+
+
 
 
 ##############################
+
+
+### attempt
+#fr=Frame(can,width=100,height=100)
+#fr.pack()
+
+#canvas.create_window(350,500, window=fr)
 #
 # блок создания 6 кнопок
-substance1 = Button(aminok,text='W', padx=100, pady=50)
+substance1 = Button(aminok)
 
-substance1.config(width=5,height=5)
+substance1.config(image=photo1)
 substance1.config(cursor='hand2')
 substance1.config(bd=8, relief=RAISED)
 substance1.config(bg='dark green', fg='white')
 substance1.config(font=('helvetica', 20, 'italic'))
-substance1.pack(padx=5, pady=5)
+substance1.pack(padx=5,pady=5)
 x1=500;
 y1=350;
 
+
+
 canvas.create_window(x1,y1, window=substance1)
 
-substance2 = Button(aminok,text='L', padx=100, pady=50)
-
-substance2.config(width=5,height=5)
+#substance2 = Button(aminok,text='L', padx=100, pady=50,wraplength=True)
+#substance2.config(width=5,height=5)
+substance2 = Button(aminok,image=photo2)
 substance2.config(cursor='hand2')
 substance2.config(bd=8, relief=RAISED)
 substance2.config(bg='dark green', fg='white')
@@ -50,9 +68,8 @@ dy=0;
 
 canvas.create_window(x1+dx,y1+dy, window=substance2)
 
-substance3 = Button(aminok,text='Q', padx=100, pady=50)
+substance3 = Button(aminok,image=photo3)
 
-substance3.config(width=5,height=5)
 substance3.config(cursor='hand2')
 substance3.config(bd=8, relief=RAISED)
 substance3.config(bg='dark green', fg='white')
@@ -64,9 +81,8 @@ dy=0;
 
 canvas.create_window(x1+dx,y1+dy, window=substance3)
 
-substance4 = Button(aminok,text='A', padx=100, pady=50)
+substance4 = Button(aminok,image=photo4)
 
-substance4.config(width=5,height=5)
 substance4.config(cursor='hand2')
 substance4.config(bd=8, relief=RAISED)
 substance4.config(bg='dark green', fg='white')
@@ -78,9 +94,9 @@ dy=350;
 
 canvas.create_window(x1+dx,y1+dy, window=substance4)
 
-substance5 = Button(aminok,text='H', padx=100, pady=50)
+substance5 = Button(aminok,image=photo5)
 
-substance5.config(width=5,height=5)
+
 substance5.config(cursor='hand2')
 substance5.config(bd=8, relief=RAISED)
 substance5.config(bg='dark green', fg='white')
@@ -93,9 +109,8 @@ dy=350;
 canvas.create_window(x1+dx,y1+dy, window=substance5)
 
 
-substance6 = Button(aminok,text='U', padx=100, pady=50)
+substance6 = Button(aminok,image=photo6)
 
-substance6.config(width=5,height=5)
 substance6.config(cursor='hand2')
 substance6.config(bd=8, relief=RAISED)
 substance6.config(bg='dark green', fg='white')
